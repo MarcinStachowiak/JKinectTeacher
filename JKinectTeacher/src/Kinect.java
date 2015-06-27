da3cb6222eccfc99c917619578f8c532ae42d818
Marcin Stachowiak <marcin.stachowiak.ms@gmail.com>
2015-06-27 13:07:09 +0200

    First commit.

---
+++ JKinectTeacher/src/Kinect.java
@@ -0,0 +1,13 @@
+
+public class Kinect {
+
+	public Kinect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Init...");
+
+	}
+
+}
